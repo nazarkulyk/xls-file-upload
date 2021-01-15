24bda836da5b431b226fdf20f6144d9cd017167d
Nazar Kulyk <nazar.kulyk@ibsolution.de>
2021-01-15 09:44:27 +0100

    fix test compiling

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,30 +1,22 @@
-import { FileUploadResult } from 'src/models';
+import { FileUploadResult } from '../models';
 import { DEFAULT_MAPPING_TYPES, EXCEL_MIME_TYPES } from '../config';
 import { UploadService } from '../upload';
+
+type File = Express.Multer.File;
 
 const TransformTypes = {
   ...DEFAULT_MAPPING_TYPES,
   Test: 'Test' as const
 };
 
-type TransformTypes = typeof TransformTypes[keyof typeof TransformTypes];
-
-type sometype<T extends TransformTypes> = T extends typeof TransformTypes.Unknown
-  ? 'Unknown'
-  : T extends typeof TransformTypes.Test
-  ? 'Test'
-  : never;
-
-// type r = sometype<typeof TransformTypes.Test>;
-
-class UploadTest extends UploadService<TransformTypes> {
+class UploadTest extends UploadService {
   // eslint-disable-next-line class-methods-use-this
-  protected validateFilePart(part: Express.Multer.File): Promise<FileUploadResult<never>> {
+  protected validateFilePart(part: File): Promise<FileUploadResult> {
     throw new Error('Method not implemented.');
   }
 
   // eslint-disable-next-line class-methods-use-this
-  protected async importDBData(type: Transforms, data: unknown, truncate = true): Promise<number> {
+  protected async importDBData(type: string, data: unknown[], truncate = true): Promise<number> {
     // eslint-disable-next-line no-console
     console.log(type, data, truncate);
     return 0;
